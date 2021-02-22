6e91daf2bbb74cebaed5f8db01b02c9b762e339b
sharanya586 <63495557+sharanya586@users.noreply.github.com>
2021-02-22 22:24:27 +0530

    Add files via upload

---
+++ h.js
@@ -0,0 +1,7 @@
+function TurnOff(){ 
+    document.getElementById('bulb').src='https://projectsassets.s3.us-east-2.amazonaws.com/Off+Bulb.png'
+   }
+   
+   function TurnOn(){ 
+    document.getElementById('bulb').src='https://projectsassets.s3.us-east-2.amazonaws.com/On+Bulb.png'
+   }
